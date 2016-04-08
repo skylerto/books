662395e3a6a1a2849ceeb526f1dd2ebd3a5775bf
Skyler Layne <skylerclayne@gmail.com>
2016-04-08 11:00:09 -0400

    books controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
+  # HOME PAGE
   # root 'welcome#index'
 
+  resources :books
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160408144924) do
+ActiveRecord::Schema.define(version: 20160408145519) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title",       limit: 255,                           null: false
@@ -21,6 +21,12 @@ ActiveRecord::Schema.define(version: 20160408144924) do
     t.string   "isbn",        limit: 255,                           null: false
     t.datetime "created_at",                                        null: false
     t.datetime "updated_at",                                        null: false
+  end
+
+  create_table "controllers", force: :cascade do |t|
+    t.string   "books",      limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
 end
