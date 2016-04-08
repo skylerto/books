4a217d8e3839c3d1f395f0d179d330e9f4352be2
Skyler Layne <skylerclayne@gmail.com>
2016-04-08 14:18:56 -0400

    cart total

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -3,6 +3,12 @@ class CartsController < ApplicationController
   def show
     cart_ids = $redis.smembers current_user_cart
     @cart_books = Book.find(cart_ids)
+    @total = 0
+    if @cart_books.size > 0
+      @cart_books.each do |book|
+        @total = @total + book.price
+      end
+    end
   end
 
   def add
@@ -15,9 +21,6 @@ class CartsController < ApplicationController
     redirect_to request.referer
   end
 
-  def empty?
-    @cart_books.size == 0
-  end
   private
   def current_user_cart
     "cart#{session.id}"
