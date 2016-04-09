c81d94f43f298f631e327f8722de9a5f11f83766
Skyler Layne <skylerclayne@gmail.com>
2016-04-09 11:27:19 -0400

    fixed misspelt container

--- db/schema.rb
+++ db/schema.rb
@@ -34,12 +34,6 @@ ActiveRecord::Schema.define(version: 20160408211042) do
     t.string   "category",    limit: 255
   end
 
-  create_table "controllers", force: :cascade do |t|
-    t.string   "books",      limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
-  end
-
   create_table "reviews", force: :cascade do |t|
     t.integer  "book_id",    limit: 4
     t.text     "review",     limit: 65535
